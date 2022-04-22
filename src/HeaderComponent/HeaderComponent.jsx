39232d695c1c74b5a30e91c81a3376d360b285a3
LeonRivera <leonriv2@gmail.com>
2022-04-21 22:10:41 -0500

    21042022 | LeonRivera | Cleaning the react project

---
+++ src/HeaderComponent/HeaderComponent.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+
+const HeaderComponent = () => {
+    return(
+        <Fragment>
+            <h1>Hola soy el header</h1>
+        </Fragment>
+    )
+}
+
+export default HeaderComponent;
