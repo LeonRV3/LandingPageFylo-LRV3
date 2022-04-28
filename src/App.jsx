88e62aeb1399ee9034cef76d3a977d9048e53c61
LeonRivera <leonriv2@gmail.com>
2022-04-28 10:56:48 -0500

    28042022 | LeonRivera | Styling section main

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import { Fragment } from 'react';
 import HeaderComponent from './HeaderComponent/HeaderComponent';
 import FooterComponent from './FooterComponent/FooterComponent';
+import MainComponent from './MainComponent/MainComponent';
 
 
 const App = () => {
   return(
     <Fragment>
       <HeaderComponent></HeaderComponent>
+      <MainComponent></MainComponent>
       <FooterComponent></FooterComponent>
     </Fragment>
   )
---
+++ src/MainComponent/MainComponent.jsx
@@ -0,0 +1,73 @@
+import { Fragment } from "react";
+import { ReactComponent as AccessAnywhereSvg } from '../images/icon-access-anywhere.svg';
+import { ReactComponent as StayProdPng } from '../images/illustration-stay-productive.png';
+import './MainComponent.css';
+
+const MainComponent = () => {
+    return (
+
+
+        <Fragment>
+
+            <main>
+
+                <div className="main-icons">
+                    <div>
+                        <AccessAnywhereSvg></AccessAnywhereSvg>
+                        <h2>
+                            Access your files, anywhere
+                        </h2>
+                        <p>
+                            The ability to use a smartphone, tablet, or computer to access your account means your
+                            files follow you everywhere.
+                        </p>
+                    </div>
+                    <div>
+                        <AccessAnywhereSvg></AccessAnywhereSvg>
+                        <h2>
+                            Access your files, anywhere
+                        </h2>
+                        <p>
+                            The ability to use a smartphone, tablet, or computer to access your account means your
+                            files follow you everywhere.
+                        </p>
+                    </div>
+                    <div>
+                        <AccessAnywhereSvg></AccessAnywhereSvg>
+                        <h2>
+                            Access your files, anywhere
+                        </h2>
+                        <p>
+                            The ability to use a smartphone, tablet, or computer to access your account means your
+                            files follow you everywhere.
+                        </p>
+                    </div>
+                    <div>
+                        <AccessAnywhereSvg></AccessAnywhereSvg>
+                        <h2>
+                            Access your files, anywhere
+                        </h2>
+                        <p>
+                            The ability to use a smartphone, tablet, or computer to access your account means your
+                            files follow you everywhere.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="main-advice">
+
+                </div>
+
+                <div className="main-opinions">
+
+                </div>
+
+            </main>
+
+        </Fragment>
+
+
+    );
+}
+
+export default MainComponent;
