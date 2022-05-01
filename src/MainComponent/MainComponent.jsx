6d59421fb32b21c4cf3f110d986564dad9da8f66
LeonRivera <leonriv2@gmail.com>
2022-05-01 11:56:20 -0500

    01052022 | LeonRivera | Added section get access

--- src/MainComponent/MainComponent.jsx
+++ src/MainComponent/MainComponent.jsx
@@ -108,6 +108,20 @@ const MainComponent = () => {
                     
                 </div>
 
+
+                <div className="main-started">
+                    <div>
+                        <h2>Get early access today</h2>
+                        <p>In only takes a minute to sign up and our free starter tier is extremely generous. If you have any
+                            questions, our support team would be happy to help you
+                        </p>
+                        <div>
+                            <input placeholder="email@example.com"></input>
+                            <button>Get Started For Free</button>
+                        </div>
+                    </div>
+                </div>
+
             </main>
 
         </Fragment>
